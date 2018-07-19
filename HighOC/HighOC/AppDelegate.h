1a5974047a6c04918d1fe4ba5ae5411c3e0dbd75
beydeng <beydeng@163.com>
2018-07-19 16:13:43 +0800

    新建项目

---
+++ HighOC/HighOC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HighOC
+//
+//  Created by DengPan on 2018/7/19.
+//  Copyright © 2018年 DengPan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
